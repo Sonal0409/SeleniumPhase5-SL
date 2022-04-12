86bbf4b1df6661917c89e5131078c02fe1cbe839
sonal0409 <mittal.sonal04@gmail.com>
2022-04-12 12:51:13 +0530

    Commit Selenium Scripts- Day2

---
+++ src/test/java/seleniumScriptsDay2/PartialLinktextDemo.java
@@ -0,0 +1,26 @@
+package seleniumScriptsDay2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PartialLinktextDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://en.wikipedia.org/wiki/Main_Page");
+		
+		
+		driver.findElement(By.partialLinkText("Tercentenary half")).click();
+		
+		
+
+	}
+
+}
